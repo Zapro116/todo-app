f87d39995274c017938f01bb17ed94b2bfaf724d
Zapro116 <43077938+Zapro116@users.noreply.github.com>
2021-12-15 17:58:36 +0530

    Add files via upload

---
+++ spring-todo-list/src/main/java/com/nttdata/ta/todo/SecurityWebApplicationInitializer.java
@@ -0,0 +1,11 @@
+package com.nttdata.ta.todo;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+    // nothing to do here
+    // web Application got initialize
+    // spring security filters is now in place
+
+}
